6a31467a5f880c91004bf82e08106b282c13484e
khj <13798187289@163.com>
2021-04-23 17:57:21 +0800

    继续合并

--- SuperHospitalDoctor/SuperHospitalDoctor/ViewController.m
+++ SuperHospitalDoctor/SuperHospitalDoctor/ViewController.m
@@ -25,8 +25,22 @@
     [but addTarget:self action:@selector(didClickedButton) forControlEvents:(UIControlEventTouchUpInside)];
     [self.view addSubview:but];
     //测试合并分支
+    
+    [self createBottomView];
 
 }
 
-
+#pragma mark -- 创建底部视图
+- (void)createBottomView {
+    UIView *bottomView = [[UIView alloc] initWithFrame:CGRectMake(0,  70, 100, 70)];
+    [self.view addSubview:bottomView];
+    
+    UIButton *bottomButton = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    bottomButton.frame = CGRectMake(40, 10, 80, 44);
+    [bottomButton setTitle:@"新建收货地址" forState:(UIControlStateNormal)];
+    bottomButton.layer.cornerRadius = 22;
+    bottomButton.backgroundColor = [UIColor redColor];
+    [bottomButton setTitleColor:[UIColor brownColor] forState:(UIControlStateNormal)];
+    [bottomView addSubview:bottomButton];
+}
 @end
